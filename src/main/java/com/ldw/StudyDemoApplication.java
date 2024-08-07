9be3222363d121d322bef91b1b42a5dff17668e3
lidawang <2632711107@qq.com>
2024-08-07 15:05:56 +0800

    项目搭建

---
+++ src/main/java/com/ldw/StudyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ldw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDemoApplication.class, args);
+    }
+
+}
